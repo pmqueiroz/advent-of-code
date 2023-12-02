e7425cfaa7da523b0ed3088cda57521d0bb09041
pmqueiroz <pedroqueiroz593@gmail.com>
2023-12-02 11:26:48 -0300

    (day 1): add part two solution

---
+++ 2023/1/part-two.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+input=$(cat ./input.txt);
+result=0;
+declare -A spelled_out_digit_map=(
+   ["one"]="1"
+   ["two"]="2"
+   ["three"]="3"
+   ["four"]="4"
+   ["five"]="5"
+   ["six"]="6"
+   ["seven"]="7"
+   ["eight"]="8"
+   ["nine"]="9"
+);
+
+function get_calibration {
+   cali=$(echo "$1" | grep -o '[0-9]' | head -n1);
+   bration=$(echo "$1" | grep -o '[0-9]' | tail -n1);
+
+   echo "$cali$bration"
+}
+
+function get_valid_substrings {
+   local string="$1";
+   local length=${#string};
+   local valid_digits="";
+
+   for ((i = 0; i < length; i++)); do
+      for ((j = i + 1; j <= length; j++)); do
+         substring="${string:i:j-i}"
+
+         if [[ "$substring" =~ ^[0-9]$ ]]; then
+            valid_digits="$valid_digits$substring";
+         else
+            for key in "${!spelled_out_digit_map[@]}"; do
+               if [[ "$substring" == "$key" ]]; then
+                  valid_digits="$valid_digits${spelled_out_digit_map[$key]}";
+               fi
+            done
+         fi
+      done
+   done
+
+   echo $valid_digits;
+}
+
+for line in ${input[@]}; do
+   valid_digits=$(get_valid_substrings $line)
+   calibration=$(get_calibration $valid_digits);
+
+   result=$(( $result + $calibration ))
+done
+
+echo $result
