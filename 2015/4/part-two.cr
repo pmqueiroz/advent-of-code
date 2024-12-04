445667576a00910dc0063de1012de9c0757c1b0e
pmqueiroz <pedroqueiroz593@gmail.com>
2024-12-04 00:36:32 -0300

    feat(2015): day 4 part two

---
+++ 2015/4/part-two.cr
@@ -0,0 +1,17 @@
+require "digest"
+
+# I'm really not proud of using brute force here,
+# but I have no idea how to solve this problem, really NO IDEA
+def run(input : String)
+  try = 0
+  loop do
+    md5 = Digest::MD5.hexdigest(input + try.to_s)
+    break if md5.starts_with?("0" * 6)
+    try += 1
+  end
+
+  puts try
+end
+
+content = File.read("#{__DIR__}/input.txt")
+run(content)
